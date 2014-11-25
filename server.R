3d8e5f28371eac132ab3e7b5030b7afa9c467760
Ward9250 <axolotlfan9250@gmail.com>
2014-11-25 01:15:53 +0000

    Finished basic app design

--- server.R
+++ server.R
@@ -1,11 +1,13 @@
 library(shiny)
+library(shinyBS)
 
 shinyServer(function(input, output) {
-  
+  # Require the HybRIDS package.
   require(HybRIDS)
-  
+  # Start up a new HybRIDS session.
   hybridsobj <- HybRIDS$new()
-  
+  # Makes sure the DNA sequences are updated.
+  # Reactive so when the DNA sequence is changed, then so is everything that depends on this.
   updateSequence <- reactive({
     validate(
       need(grepl(".fas", input$fastafile$name) || grepl(".fasta", input$fastafile$name), "Provide a FASTA format sequence file.")
@@ -21,14 +23,98 @@ shinyServer(function(input, output) {
     hybridsobj$DNA$htmlSummary()
   })
   
+  # Gets HybRIDS to update the settings for generating triplets and update the triplets that are to be generated and analyzed.
+  updateTripletGenSettings <- reactive({
+    input$comboGen
+    newoptions <- c()
+    if(isolate(input$betweengroups)){
+      newoptions <- c(newoptions, 1L)
+    }
+    if(isolate(input$distancebased)){
+      if(isolate(input$distancemethod) == "yes"){
+        newoptions <- c(newoptions, 3L)
+      } else {
+        newoptions <- c(newoptions, 2L)
+      }
+    }
+    validate(
+      need(!is.null(newoptions), "Select either to generate triplet combinations based on group specification or based on distance, or both.")
+    )
+    validate(
+      need(hybridsobj$DNA$hasDNA(), "No sequence file is loaded into HybRIDS.")
+    )
+    groups <- lapply(unlist(lapply(1:isolate(input$numGroups), function(i) paste0("group", i))), function(n) isolate(input[[n]]))
+    groups[sapply(groups, is.null)] <- NULL
+    hybridsobj$setParameters("TripletGeneration",
+                             Method = newoptions,
+                             DistanceThreshold = isolate(input$mandistthreshold),
+                             PartitionStrictness = as.integer(isolate(input$partitionStrictness)),
+                             Groups = groups)
+  })
+  
+  output$NumCombos <- renderText({
+    updateTripletGenSettings()
+    length(hybridsobj$comparrisonSettings$AcceptedCombinations)
+  })
+  
+  output$GeneratedTriplets <- renderText({
+    updateTripletGenSettings()
+    hybridsobj$comparrisonSettings$htmlCombinations()
+    })
+  
   output$tripletgen <- renderUI({
     updateSequence()
-    validate(
-      need(input$numGroups >= 1, "Enter how many sequence groups there are.")
-    )
     lapply(1:input$numGroups, function(i) {
       selectInput(inputId=paste0("group", i), label=paste0("Sequence Group ", i), choices = hybridsobj$DNA$getSequenceNames(), multiple=TRUE)
     })
   })
-
+  
+  # Server functions for the analyze triplets page.
+  output$TripletSelector <- renderUI({
+    updateTripletGenSettings()
+    selectInput("tripletSelection", tags$strong("Selected Triplet"), hybridsobj$triplets$tripletCombinations())
+  })
+  
+  currentTripletSelection <- reactive({
+    return(unlist(strsplit(input$tripletSelection, ", ")))
+  })
+  
+  scanSeqSim <- reactive({
+    hybridsobj$setParameters("SSAnalysis", WindowSize = as.integer(input$windowSize),
+                             StepSize = as.integer(input$stepSize))
+    hybridsobj$analyzeSS(currentTripletSelection())
+  })
+  
+  findBlocks <- reactive({
+    scanSeqSim()
+    hybridsobj$setParameters("BlockDetection", ManualThresholds = input$manBlockDetectDist,
+                             AutoThresholds = (input$detectionMethod == "no"),
+                             ManualFallback = input$fallbackManual)
+    hybridsobj$findBlocks(currentTripletSelection())
+  })
+  
+  dateBlocks <- reactive({
+    dateanyway <- !input$eliminateinsignificant
+    hybridsobj$setParameters("BlockDating", MutationRate = input$mu, PValue = input$alpha,
+                             BonfCorrection = input$bonf, DateAnyway = dateanyway,
+                             MutationCorrection = input$correctionModel)
+    hybridsobj$dateBlocks(currentTripletSelection())
+  })
+  
+  output$barsPlot <- renderPlot({
+    scanSeqSim()
+    print(hybridsobj$plotTriplets(currentTripletSelection(), What="Bars"))
+  })
+  
+  output$linesPlot <- renderPlot({
+    scanSeqSim()
+    print(hybridsobj$plotTriplets(currentTripletSelection(), What="Lines"))
+  })
+  
+  output$blocksTable <- renderDataTable({
+    findBlocks()
+    dateBlocks()
+    hybridsobj$tabulateDetectedBlocks(currentTripletSelection(), Neat=TRUE)
+  })
+  
 })
--- ui.R
+++ ui.R
@@ -1,9 +1,10 @@
 library(shiny)
+library(shinyBS)
 
 shinyUI(navbarPage("HybRIDS",
                    
                    tabPanel("Sequence Data",
-                            HTML("<h1>Input DNA sequence file.</h1>"),
+                            HTML("<h1>Input DNA sequence file.</h1><hr>"),
                             HTML("<p>Upload your FASTA file containing sequences from your different populations to get started.</p>"),
                             HTML("<p>Note: Duplicate Sequences will be removed.</p>"),
                             HTML("<hr><div align='center'>"),
@@ -19,19 +20,80 @@ shinyUI(navbarPage("HybRIDS",
                             htmlOutput("SeqInfo")
                             ),
                    
-                   tabPanel("Selecting Comparrisons",
-                            HTML("<h1>Specify sequence comparrisons to scan.</h1>"),
-                            sidebarLayout(
-                              sidebarPanel(radioButtons("combomethod", "Triplet Generation Method:",
-                                                       c("All possible triplets" = "all",
-                                                         "Between Groups" = "between")),
-                                           conditionalPanel("input.combomethod == 'between'",
-                                                            numericInput("numGroups", "How many groups?", value=3, min=1),
-                                                            uiOutput("tripletgen"))
-                                           ),
-                              mainPanel("Ho!")
-                              
-                              )
-                            
-                            )
-))
+                   tabPanel("Specify Triplets to Analyze",
+                            HTML("<h1>Specify sequence combinations to scan.</h1><hr>"),
+                            fluidRow(column(6,
+                                            h3("Specify what triplets to scan:"),
+                                            "By default, every possible combination of 3 sequences will be analyzed.",
+                                            "You can refine this by eliminating triplets containing sequences that are insufficiently diverged.",
+                                            "",
+                                            inputPanel(
+                                              checkboxInput("betweengroups", "Compare between groups", value=TRUE),
+                                              checkboxInput("distancebased", "Raw p-distance based"),
+                                              conditionalPanel("input.betweengroups",
+                                                               h4("Group Options"),
+                                                               numericInput("numGroups", "How many groups?", value=3, min=1),
+                                                               radioButtons("partitionStrictness", "How many sequences from the same group are allowed in a triplet?",
+                                                                            c("One" = 1L,
+                                                                              "Two" = 2L), selected=2L),
+                                                               uiOutput("tripletgen")),
+                                              conditionalPanel("input.distancebased",
+                                                               h4("Distance based elimination options."),
+                                                               radioButtons("distancemethod", "Use automatic threshold detection?",
+                                                                            c("Yes" = "yes",
+                                                                              "No, specify a raw p-distance to use as a threshold manually." = "no")),
+                                              conditionalPanel("input.distancemethod == 'no'",
+                                                               sliderInput("mandistthreshold", "Raw p-distance threshold.", 0.01, 1, value=0.01, step=0.01))),
+                                              actionButton("comboGen", "Generate Combinations to Scan")
+                                              )  
+                                     ),
+                                     column(6,
+                                            fluidRow(column(6, HTML("<b>Number of combinations to scan: </b>"),
+                                                            textOutput("NumCombos"),
+                                                            HTML("</br><b>Combinations to scan:</b><br>"),
+                                                            htmlOutput("GeneratedTriplets")))
+                                            ))),
+                   
+                   tabPanel("Analyze Triplets",
+                            h1("Analyze and Explore Sequence Triplets"),
+                            HTML("<hr>"),
+                            fluidRow(
+                              column(4,
+                                     inputPanel(
+                                       h4("How to scan sequence similarity:"),
+                                       numericInput("windowSize", "Size of sliding window (in bp):", min=1, value=100),
+                                       numericInput("stepSize", "Step size of sliding window (in bp):", min=1, value=1)),
+                                     inputPanel(
+                                       uiOutput("TripletSelector")
+                                       )),
+                              column(4,
+                                     inputPanel(
+                                       h4("Block detection settings:"),
+                                       radioButtons("detectionMethod", "Use a manual threshold?", c("Yes" = "yes", "No, automatically decide thresholds from data." = "no"), selected="no"),
+                                       checkboxInput("fallbackManual", "Fallback to a manual threshold?"),
+                                       conditionalPanel("input.detectionMethod == 'yes' || input.fallbackManual", 
+                                                        sliderInput("manBlockDetectDist", "Sequence Similarity Threshold:", 1, 100, value=95, step=1))
+                                     )),
+                              column(4,
+                                     inputPanel(
+                                       h4("Block dating settings:"),
+                                       numericInput("mu", "Mutation Rate:", 10e-8),
+                                       numericInput("alpha", "Critical Value (alpha):", 0.05),
+                                       checkboxInput("bonf", "Bonferoni Correct Critical Value", TRUE),
+                                       checkboxInput("eliminateinsignificant", "Eliminate insignificant blocks", TRUE),
+                                       selectInput("correctionModel", "Mutation correction model:", c("JC69", "K80", "F81",
+                                                                                                      "K81", "F84", "BH87",
+                                                                                                      "T92", "TN93", "GG95"))))),
+                            tags$hr(),
+                            bsCollapse(multiple = FALSE, open = "col1", id = "collapse1",
+                                       bsCollapsePanel("Sequence Similarity, RGB plot",
+                                                       plotOutput("barsPlot"),
+                                                       id="col1",
+                                                       value="test1"),
+                                       bsCollapsePanel("Sequence Similarity, lines plot",
+                                                       plotOutput("linesPlot"),
+                                                       id="col2",
+                                                       value="test2")
+                            ),
+                            dataTableOutput("blocksTable")                    
+)))
